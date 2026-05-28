20fcf43644a3d0d0faaa8a5a9fd91321bb9ef734
devtarj <tarj.mehta@icloud.com>
2026-05-28 16:25:08 -0400

    minor change

--- PowerShell Scripts/10-detection-dotNet-update.ps1
+++ PowerShell Scripts/10-detection-dotNet-update.ps1
@@ -1,7 +1,7 @@
 # ============================================
 # Reliable .NET Detection Script
 # Intune Enterprise Version
-# last edit: 2026-05-22
+# last edit: 2026-05-28
 # ============================================
 
 $LogPath = "C:\ProgramData\Company\Logs"
