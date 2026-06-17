d0cc53579877a28fff43715ac24fb699cb796ce1
devtarj <tarj.mehta@icloud.com>
2026-06-17 09:21:51 -0400

    testing path

--- PowerShell Scripts/dotNET_update_troubleshooting/collect_diagnostics.ps1
+++ PowerShell Scripts/dotNET_update_troubleshooting/collect_diagnostics.ps1
@@ -1,22 +1,8 @@
-$Log = "C:\ProgramData\DotNetDiagnostic.txt"
-
-$Results = @()
-
-$Results += "===== WHOAMI ====="
-$Results += (whoami)
-
-$Results += ""
-$Results += "===== DOTNET RUNTIMES ====="
-$Results += (dotnet --list-runtimes 2>&1)
-
-$Results += ""
-$Results += "===== DOTNET SDKS ====="
-$Results += (dotnet --list-sdks 2>&1)
-
-$Results += ""
-$Results += "===== WINGET UPGRADES ====="
-$Results += (winget upgrade 2>&1)
-
-$Results | Out-File $Log -Force
-
-$Results
+if (Test-Path "C:\ProgramData\Company\Logs\DotNetUpdate.log")
+{
+    Write-Output "LOG_EXISTS"
+}
+else
+{
+    Write-Output "LOG_MISSING"
+}
