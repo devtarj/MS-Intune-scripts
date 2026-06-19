64b0715f275ad979ef1e56d1f3b2a84b1f3b1ac5
devtarj <tarj.mehta@icloud.com>
2026-06-19 09:40:38 -0400

    one time script

--- PowerShell Scripts/dotNET_update_troubleshooting/diagnostics_2.ps1
+++ PowerShell Scripts/dotNET_update_troubleshooting/diagnostics_2.ps1
@@ -1,1 +1,10 @@
-dotnet --list-runtimes
+$packages = @(
+    "Microsoft.DotNet.Runtime.8",
+    "Microsoft.DotNet.AspNetCore.8",
+    "Microsoft.DotNet.SDK.8"
+)
+
+foreach ($pkg in $packages) {
+    Write-Output "===== $pkg ====="
+    winget list --id $pkg
+}
