7c42b62b84a1af8e67498e067a54fb31a5c8c585
devtarj <tarj.mehta@icloud.com>
2026-08-03 12:57:52 -0400

    test winget silent script

---
+++ PowerShell Scripts/MISC Test/winget.ps1
@@ -0,0 +1,8 @@
+$before = (Get-Process).Id
+
+winget upgrade --all --silent --accept-source-agreements --accept-package-agreements
+
+Start-Sleep -Seconds 5
+
+Get-Process | Where-Object { $before -notcontains $_.Id -and $_.MainWindowHandle -ne 0 } |
+    Stop-Process -Force
