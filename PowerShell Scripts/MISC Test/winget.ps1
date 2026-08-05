0ec745484f7fa82cf76004e6c689163997e8b2d8
devtarj <tarj.mehta@icloud.com>
2026-08-05 11:46:47 -0400

    added message in case of failure

--- PowerShell Scripts/MISC Test/winget.ps1
+++ PowerShell Scripts/MISC Test/winget.ps1
@@ -1,8 +1,18 @@
-$before = (Get-Process).Id
+try {
+    $before = (Get-Process).Id
 
-winget upgrade --all --silent --accept-source-agreements --accept-package-agreements --include-unknown
+    winget upgrade --all --silent --accept-source-agreements --accept-package-agreements
+    $wingetExit = $LASTEXITCODE
 
-Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 5
 
-Get-Process | Where-Object { $before -notcontains $_.Id -and $_.MainWindowHandle -ne 0 } |
-    Stop-Process -Force
+    Get-Process | Where-Object { $before -notcontains $_.Id -and $_.MainWindowHandle -ne 0 } |
+        Stop-Process -Force
+
+    Write-Output "Upgrade completed. Winget exit code: $wingetExit"
+    exit 0
+}
+catch {
+    Write-Output "FAILED: $($_.Exception.Message)"
+    exit 1
+}
